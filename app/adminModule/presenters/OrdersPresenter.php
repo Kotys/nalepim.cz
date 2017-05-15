2e9971ad244eafc8c87fe15f944d630f9779870e
Jan Kotrba <jan.kotrba@b2a.cz>
2017-05-16 01:23:38 +0200

    Orders view

--- app/adminModule/presenters/OrdersPresenter.php
+++ app/adminModule/presenters/OrdersPresenter.php
@@ -6,8 +6,76 @@
 namespace App\AdminModule\Presenters;
 
 use App\AdminModule\SecuredAdminPresenter;
+use Nette\Application\UI\Form;
 
-class OrdersPresenter  extends SecuredAdminPresenter
+/**
+ * Class OrdersPresenter
+ * @package App\AdminModule\Presenters
+ */
+class OrdersPresenter extends SecuredAdminPresenter
 {
+	/**
+	 * @persistent
+	 */
+	public $awaitingPayment = true;
 
+	/**
+	 * @persistent
+	 */
+	public $awaitingExpedition = true;
+
+	/**
+	 * @persistent
+	 */
+	public $resolved = false;
+
+	/**
+	 * @persistent
+	 */
+	public $canceledTest = false;
+
+	/**
+	 * @persistent
+	 */
+	public $searchTerm;
+
+	/**
+	 *
+	 */
+	public function actionDefault()
+	{
+		$this->getTemplate()->awaitingPayment = $this->awaitingPayment;
+		$this->getTemplate()->awaitingExpedition = $this->awaitingExpedition;
+		$this->getTemplate()->resolved = $this->resolved;
+		$this->getTemplate()->canceledTest = $this->canceledTest;
+	}
+
+	/**
+	 * @param $filterKey
+	 */
+	public function handleChangeFilterValue($filterKey)
+	{
+		if(isset($this->$filterKey)) {
+			$this->$filterKey = !$this->$filterKey;
+		}
+
+		$this->redirect('this');
+	}
+
+	/**
+	 * @return Form
+	 */
+	public function createComponentSearchForm() {
+		$form = new Form();
+		$form->addText('searchTerm')->setDefaultValue($this->searchTerm);
+		$form->addSubmit('find');
+
+		$form->onSuccess[] = function(Form $form, $formValues) {
+			$this->redirect('this', [
+				'searchTerm' => $formValues->searchTerm
+			]);
+		};
+
+		return $form;
+	}
 }
